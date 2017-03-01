550c14462118c935a8213616312b63a12e3d62a7
sturple <shawn.turple@shaw.ca>
2017-02-28 17:26:08 -0800

    Updating styles. updated functions.php

--- functions.php
+++ functions.php
@@ -4,4 +4,12 @@ add_action( 'wp_enqueue_scripts', function(){
    wp_enqueue_style('theme',get_stylesheet_directory_uri().'/assets/css/style.css');    
 }, 20);
 
+add_filter('the_content',function($content){
+	// don't want this annoying p tag wrap and br tag. except for blog posts.
+	if ((get_post_type() === 'page') and ( in_array( get_the_ID(), array(2) )) ){		
+		remove_filter( 'the_content', 'wpautop' );
+	}
+	return $content;
+},2);
+
 ?>
